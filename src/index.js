45af4caf1dbdceb5e200dd9df051af1c11833d77
Liza McLain <lizamclain@gmail.com>
2022-12-15 08:35:20 -0600

    Initial commit with pretty much everything

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let priority = document.createElement('select')
+  let option = document.createElement('option')
+  option.textContent = 'select priority'
+  priority.add(new Option('low')),
+  priority.add(new Option('medium')),
+  priority.add(new Option('high'))
+
+  // function priorityColors(priority) {
+  //   if (option.value === 'low') {
+  //     console.log('red')
+  //   }
+  // }
+  // priorityColors();
+
+  createTaskForm.appendChild(priority)
+  priority.appendChild(option)
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    createToDo(e.target.newTask.value);
+    form.reset();
+  });
 });
+
+function createToDo(newTask){
+  let ul = document.getElementById('tasks')
+  let btn = document.createElement('button')
+  let li = document.createElement('li');
+  btn.addEventListener('click', markComplete)
+  li.textContent = `${newTask} `
+  btn.textContent = "X"
+  ul.appendChild(li)
+  li.appendChild(btn)
+}
+
+function markComplete(e){
+  e.target.parentNode.remove()
+}
